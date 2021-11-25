3972cf54f3c16136e2e60b3a6a61034f64b1ea4f
Haggai Kaunda <haggaikaunda@berkeley.edu>
2021-11-25 01:00:35 -0700

    refactor: simplify and convert HTMLDocumentList into a java array after invoking getElementsByClassName

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
+const identifiers = {
+  class: {
+    mode: "mode",
+    selected: "selected",
+  },
+  id: {
+    colorDisplay: "color-display",
+    r: "r",
+    g: "g",
+    b: "b",
+  },
+};
+
 const headerIds = ["color-display", "r", "g", "b"];
 const [header, r, g, b] = headerIds.map((id) => document.getElementById(id));
 
-let levels = getLevels();
-const gameLevel = levels.find((level) => undefined);
+let levels = [...document.getElementsByClassName("mode")];
+
+// game mode.
+const gameLevel = levels.find((level) => {
+  const classList = [...level.classList];
+  return classList.includes(identifiers.class.selected);
+}).innerHTML;
+
+// set up event listeners to toggle between game modes.
 levels.forEach((level) => {
   level.addEventListener("click", function () {
     levels.forEach((mode) => mode.classList.remove("selected"));
     this.classList.add("selected");
   });
 });
-
-function getLevels() {
-  const [x, y] = document.getElementsByClassName("mode");
-  return [x, y];
-}
-
-// toggleLevel();
