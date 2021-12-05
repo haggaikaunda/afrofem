9f244e0395f882960710b7d88e38ebc635f1f248
Haggai Kaunda <haggaikaunda@berkeley.edu>
2021-12-05 11:31:24 -0800

    add functionality to the start button so that clicking it generates random colors for each square

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const gElement = document.getElementById("g");
 const bElement = document.getElementById("b");
 
 const levels = Array.from(document.getElementsByClassName("mode"));
+const squares = Array.from(document.getElementsByClassName("square"));
 
 let selectedLevelButton = levels.find((level) => {
   const classList = Array.from(level.classList);
@@ -18,4 +19,22 @@ levels.forEach((level) => {
 
     gameLevel = this.innerHTML;
   });
+});
+
+// Attempt to make all the square have background color: rgb(200, 45, 255)
+const startButton = document.getElementById("reset");
+
+startButton.addEventListener("click", function () {
+  for (let i = 0; i < squares.length; i = i + 1) {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const rgbString = "rgb(" + red + "," + green + "," + blue + ")";
+    console.log(rgbString);
+
+    const square = squares[i];
+
+    square.style.backgroundColor = rgbString;
+  }
 });
